159115dfe110e5fef59718459cedc4251250e42e
gdambor <glenioborges@gmail.com>
2019-11-05 10:05:52 -0500

    new commit

--- src/main/java/io/pivotal/democloud/DemoCloudApplication.java
+++ src/main/java/io/pivotal/democloud/DemoCloudApplication.java
@@ -2,12 +2,20 @@ package io.pivotal.democloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCloudApplication.class, args);
+	}
+
+	@RequestMapping("/hi")
+	public String hi() {
+		return "Hello!!!";
 	}
 
 }
